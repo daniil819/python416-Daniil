9a2a1b60f29933c4c540d6814b53c38f4e8bf70b
GarisMod888 <daniilagapov740@gmail.com>
2025-03-25 21:32:28 +0300

    open file

--- Lesson 21.py
+++ Lesson 21.py
@@ -1,2 +1,23 @@
 # print("Проверка")
-print("Новое устройство")
+# print("Новое устройство")
+
+# Работа с файлами
+
+# f = open('text.txt') # Относительный путь
+
+b = open(r'C:\Users\Garis mod\Desktop\Python\text.txt', "r")  # Абсолютный путь
+# print(*f)
+# print(*b)
+# print(f)
+# print(f.mode)
+# print(b.mode)
+
+# print(f.name)
+# print(b.name)
+# print(b.encoding)
+#
+# b.close()
+# print(b.closed)
+
+print(b.read(3))
+b.close()
