abf4c6e4605c461d8223d1561e9101657b70f357
GarisMod888 <daniilagapov740@gmail.com>
2025-07-15 00:28:29 +0300

    dz

--- DZsite/personal_portfolio/skills/views.py
+++ DZsite/personal_portfolio/skills/views.py
@@ -5,3 +5,5 @@ from .models import Skills
 def index(request):
     projects = Skills.objects.all()
     return render(request, 'skills/index.html', {'projects': projects})
+
+
--- third/todos/todos/urls.py
+++ third/todos/todos/urls.py
@@ -22,9 +22,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     # Auth (регистрация и авторизация)
-    path('signup/', views.signup_user, name="signupuser"),
+    path('signup/', views.signup_user, name='signupuser'),
     path('logout/', views.logout_user, name='logoutuser'),
+    path('login/', views.login_user, name='loginuser'),
 
-    # Todos (Вывод на страницу с постановкой задач)
-    path('current/', views.current_todos, name='currenttodos'),
+    # Todos (постановка задач)
+    path('current/', views.current_todos, name="currenttodos"),
+    path('', views.home, name="home"),
+    path('create/', views.create_todo, name='createtodo'),
+    path('todo/<int:todo_pk>', views.view_todo, name='viewtodo'),
 ]
