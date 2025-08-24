c31c90b965f4f12e23c0109df1765136ab4449d3
GarisMod888 <daniilagapov740@gmail.com>
2025-08-24 16:49:48 +0300

    diplom

--- Diplom/personal_portfolio/personal_portfolio/urls.py
+++ Diplom/personal_portfolio/personal_portfolio/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.conf.urls.static import static
 from view import views
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +26,5 @@ urlpatterns = [
     path('menu/', views.menu, name='menu'),
     path('personal/', views.personal, name='personal'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
