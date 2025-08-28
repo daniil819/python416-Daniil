d41dc52f214c6c49ef2d94a96127981192d5e365
GarisMod888 <daniilagapov740@gmail.com>
2025-08-28 14:52:50 +0300

    diplom

--- fourth/devsearch/projects/utils.py
+++ fourth/devsearch/projects/utils.py
@@ -3,42 +3,44 @@ from django.db.models import Q
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 
+def paginate_projects(request, pr, results):
+    page = request.GET.get('page')
+    # results = 3
+    paginator = Paginator(pr, results)
+
+    try:
+        pr = paginator.page(page)
+    except PageNotAnInteger:  # http://127.0.0.1:8000/projects/?page=dfdfsd
+        page = 1
+        pr = paginator.page(page)
+    except EmptyPage:  # http://127.0.0.1:8000/projects/?page=1000
+        page = paginator.num_pages
+        pr = paginator.page(page)
+
+    left_index = int(page) - 4
+
+    if left_index < 1:
+        left_index = 1
+
+    right_index = int(page) + 5
+
+    if right_index > paginator.num_pages:
+        right_index = paginator.num_pages + 1
+
+    custom_range = range(left_index, right_index)
+    return custom_range, pr
+
+
 def search_projects(request):
     search_query = ''
 
     if request.GET.get('search_query'):
         search_query = request.GET.get('search_query')
 
-    pr = Project.objects.filter(  # Запрос к базе данных и поиск
+    pr = Project.objects.filter(
         Q(title__icontains=search_query) |
         Q(description__icontains=search_query) |
         Q(owner__name__icontains=search_query)
     )
 
     return pr, search_query
-
-
-def paginate_projects(request, pr, results):
-    page = request.GET.get('page')
-    # results = 3  # Указали количество проектов на 1 странице
-    paginator = Paginator(pr, results)
-    # обработка случаев
-    try:
-        pr = paginator.page(page)
-    except PageNotAnInteger:
-        page = 1  # Выводит на первую страницу
-        pr = paginator.page(page)
-    except EmptyPage:
-        page = paginator.num_pages  # Выводит на последнюю страницу
-        pr = paginator.page(page)
-
-    left_index = int(page) - 4
-    if left_index < 1:
-        left_index = 1
-
-    right_index = int(page) + 5
-    if right_index > paginator.num_pages:
-        right_index = paginator.num_pages + 1
-
-    custom_range = range(1, right_index)
-    return custom_range, pr
