6168ac616c183b5b3e25dcc45356f5ac07d53309
GarisMod888 <daniilagapov740@gmail.com>
2025-09-11 16:14:27 +0300

    diplom

--- fifth/programming_blog/blog/views.py
+++ fifth/programming_blog/blog/views.py
@@ -1,31 +1,59 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, CreateView, FormView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
+
+from .forms import *
 
 from .models import *
 
-menu = [
-    {'title': "Добавить статью", "url_name": 'index'},
-    {'title': "Войти", "url_name": 'index'}
-]
+from .utils import *
 
 
-class BlogHome(ListView):
+class LoginUser(DataMixin,  LoginView):
+    form_class = AuthenticationForm
+    template_name = "blog/login.html"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Авторизация")
+        return dict(list(context.items()) + list(c_def.items()))
+
+
+class RegisterUser(DataMixin, CreateView):
+    form_class = RegisterUserForm
+    template_name = "blog/register.html"
+    # success_url = reverse_lazy('login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Регистрация")
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('index')
+
+
+class BlogHome(DataMixin, ListView):
     model = Blog
     template_name = "blog/index.html"
     context_object_name = "posts"
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = "Главная страница"
-        context['cat_selected'] = 0
-        context['menu'] = menu
-        return context
+        c_def = self.get_user_context(title="Главная страница")
+        return dict(list(context.items()) + list(c_def.items()))
 
     def get_queryset(self):
         return Blog.objects.filter(is_published=True).select_related("cat")
 
 
-class ShowPost(DetailView):
+class ShowPost(DataMixin, DetailView):
     model = Blog
     template_name = "blog/post.html"
     slug_url_kwarg = "post_slug"
@@ -33,12 +61,47 @@ class ShowPost(DetailView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = context['post']
-        context['menu'] = menu
-        return context
+        c_def = self.get_user_context(title=context['post'])
+        return dict(list(context.items()) + list(c_def.items()))
 
 
-class BlogCategory(ListView):
+class BlogCategory(DataMixin, ListView):
     model = Blog
     template_name = "blog/index.html"
     context_object_name = "posts"
+
+    def get_queryset(self):
+        return Blog.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True).select_related("cat")
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c = Category.objects.get(slug=self.kwargs['cat_slug'])
+        c_def = self.get_user_context(title="Категория - " + str(c.name), cat_selected=c.pk)
+        return dict(list(context.items()) + list(c_def.items()))
+
+
+class AddPage(LoginRequiredMixin, DataMixin, CreateView):
+    form_class = AddPostForm
+    template_name = 'blog/addpage.html'
+    success_url = reverse_lazy('index')
+    login_url = reverse_lazy('index')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Добавление статьи")
+        return dict(list(context.items()) + list(c_def.items()))
+
+
+class ContactFormView(DataMixin, FormView):
+    form_class = ContactForm
+    template_name = 'blog/contact.html'
+    success_url = reverse_lazy('index')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Обратная связь")
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return redirect('index')
--- fifth/programming_blog/programming_blog/urls.py
+++ fifth/programming_blog/programming_blog/urls.py
@@ -22,6 +22,8 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('captcha/', include('captcha.urls')),
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
